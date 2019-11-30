b678a034da9b8ea206e5ecc81fb0105bc10dd03c
divay pandey <divay.pandey@outlook.com>
2019-11-30 14:29:42 -0600

    Signed-off-by: divay pandey <divay.pandey@outlook.com>

---
+++ Assets/Code/cannonFire.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cannonFire : MonoBehaviour
+{
+    public GameObject shell;
+    public float speed = 1000f;
+    public float force = 700f;
+    Rigidbody rb;
+    Transform t;
+    float tiltAngle = 60.0f;
+    public AudioSource boom;
+    public float spWait;
+
+    public float spMWait = 2;
+    public float spLWait = 6;
+    public float beginWait = 2;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        t = GetComponent<Transform>();
+        StartCoroutine(coro());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        spWait = Random.Range(spLWait,spMWait);
+
+    }
+
+    IEnumerator coro()
+    {
+        yield return new WaitForSeconds(beginWait);
+        while(true)
+        {
+            /*GameObject bullet = Instantiate(shell,transform.position,Quaternion.identity) as GameObject;
+            Rigidbody body = bullet.GetComponent<Rigidbody>();
+            body.velocity += this.transform.forward * speed * Time.deltaTime;
+            body.AddForce(Vector3.forward * speed);
+            //Debug.Log("We have waited for");*/
+            float tiltAroundX = Input.GetAxis("Vertical") * tiltAngle;
+            float tiltAroundZ = Input.GetAxis("Horizontal") * tiltAngle;
+
+        Quaternion target = Quaternion.Euler(tiltAroundX, 0, tiltAroundZ);
+
+        // Dampen towards the target rotation
+            GameObject newBullet = GameObject.Instantiate(shell, t.transform.position, t.transform.rotation) as GameObject;
+            newBullet.GetComponent<Rigidbody>().velocity += Vector3.up * 5;
+            newBullet.GetComponent<Transform>().rotation = Quaternion.Slerp(t.rotation, target,  Time.deltaTime * 5.0f);
+            boom.Play();
+            newBullet.GetComponent<Rigidbody>().AddForce(newBullet.transform.forward * 1000);
+            yield return new WaitForSeconds(spWait);
+        }
+
+    }
+}
---
+++ Assets/Code/shell.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shell : MonoBehaviour
+{
+
+    public AudioSource boom;
+
+    void Start()
+    {
+        //boom.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
